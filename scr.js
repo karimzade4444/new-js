8586260cf95f66e8b190f37a2c4396b673ddc7a8
Karimzade_8222 <karimzademustafa@gmail.com>
2026-03-16 20:18:20 +0500

    Task new DOM

---
+++ scr.js
@@ -0,0 +1,14 @@
+const cards = [
+  { id: 1, name: "Apple", color: "red" },
+  { id: 2, name: "Banana", color: "yellow" },
+  { id: 3, name: "Grape", color: "purple" }
+];
+
+let sc = document.querySelector(".sc")
+function render(){
+    cards.forEach((el)=>{
+    let div = document.createElement("div")
+    let box = 
+
+    })
+}
