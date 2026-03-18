57ecaa656e38c40172b6cec54b48ca5abb24a8d7
Karimzade_8222 <karimzademustafa@gmail.com>
2026-03-18 20:12:30 +0500

    X

--- scr.js
+++ scr.js
@@ -1,49 +1,25 @@
-const cards = [
+let cards = [
   { id: 1, name: "Apple", color: "red" },
   { id: 2, name: "Banana", color: "yellow" },
-  { id: 3, name: "Grape", color: "purple" }
+  { id: 3, name: "Grape", color: "purple" },
 ];
 
 let container = document.querySelector(".container");
 
-container.style.display = "flex";
-container.style.gap = "20px";
-container.style.flexWrap = "wrap";
+function render() {
+  container.innerHTML = "";
+  cards.forEach((el) => {
+    let div = document.createElement("div");
+    div.style.backgroundColor = el.color;
+    const pName = document.createElement("p");
+    pName.textContent = el.name;
+    div.append(pName);
+    container.append(div);
+  });
+}
 
-cards.forEach((card) => {
-  let div = document.createElement("div");
-  let p = document.createElement("p");
-  let likeBtn = document.createElement("button");
-  let deleteBtn = document.createElement("button");
+function handleDeleteFruit(id) {
+  cards = cards.filter((el) => el.id != id);
+}
 
-  div.style.border = "1px solid black";
-  div.style.padding = "10px";
-  div.style.cursor = "pointer";
-
-  p.textContent = card.name;
-
-  let likes = 0;
-  likeBtn.textContent = "❤️ 0";
-
-  deleteBtn.textContent = "❌ Delete";
-
-  div.append(p, likeBtn, deleteBtn);
-  container.append(div);
-
-  div.onclick = () => {
-    if (div.style.backgroundColor === card.color) {
-      div.style.backgroundColor = "white";
-    } else {
-      div.style.backgroundColor = card.color;
-    }
-  };
-  likeBtn.onclick = (e) => {
-    e.stopPropagation();
-    likes++;
-    likeBtn.textContent = "❤️ " + likes;
-  };
-  deleteBtn.onclick = (e) => {
-    e.stopPropagation();
-    div.remove();
-  };
-});
+render();
